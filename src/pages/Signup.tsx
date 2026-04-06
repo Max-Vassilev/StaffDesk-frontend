35de290ad978e5a12d872b1df4224e2bbe7de2f0
Maxim Vassilev <maxim@Maxims-MacBook-Air.local>
2026-04-06 15:09:47 +0300

    Small CSS fixes

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,11 +1,11 @@
-import { useEffect } from 'react'
+import { useLayoutEffect } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import './Signup.css'
 
 const Signup: React.FC = () => {
-  useEffect(() => {
-    window.scrollTo(0, 0)
+  useLayoutEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'instant' })
   }, [])
 
   return (
